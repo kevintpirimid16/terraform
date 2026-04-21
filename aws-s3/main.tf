6969054fefa6cad27369a3dd768cc8049bb7ab1f
kevintpirimid16 <kevin.tamakuwala@pirimidtech.com>
2026-04-21 22:51:24 +0530

    add: create aws s3 bucket using tf

--- aws-s3/main.tf
+++ aws-s3/main.tf
@@ -11,12 +11,6 @@ provider "aws" {
   # Configuration options
   region = var.region
 }
-
-resource "aws_instance" "myserver" {
-  ami = "ami-0705384c0b33c194c"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "Server01"
-  }
+resource "aws_s3_bucket" "mys3bucket" {
+  bucket = "mys3bucket123"
 }
