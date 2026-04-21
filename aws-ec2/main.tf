af50ae50ce44f81c957eebe8408cec905e556df9
kevintpirimid16 <kevin.tamakuwala@pirimidtech.com>
2026-04-21 20:02:47 +0530

    add: basic ec2 instance using tf

---
+++ aws-ec2/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.41.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "eu-north-1"
+}
+
+resource "aws_instance" "myserver" {
+  ami = "ami-0705384c0b33c194c"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Server01"
+  }
+}
